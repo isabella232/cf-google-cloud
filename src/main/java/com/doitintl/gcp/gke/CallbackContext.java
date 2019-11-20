69357ab7bbbe7c4e2cba69988f8224ebca7dfaf6
avivl <aviv.laufer@gmail.com>
2019-11-20 08:54:46 +0200

    Initial commit

---
+++ src/main/java/com/doitintl/gcp/gke/CallbackContext.java
@@ -0,0 +1,12 @@
+package com.doitintl.gcp.gke;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+public class CallbackContext {
+
+}
